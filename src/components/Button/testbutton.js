7b86dd3694ce7731a4bd21d52ddac018933230b4
NicholasGray <nicholas_gray@live.com>
2021-03-17 10:11:18 -0400

    Update testbutton.js

--- src/components/Button/testbutton.js
+++ src/components/Button/testbutton.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const Button = () => (
-   <button stretched={true} type="primary" mode="filled" size="s"> Merge! </button>
- );
-  
- export { Button as default }; 
+const Button = () => ( <button stretched={true} type="button" mode="filled" size="s"> Merge! </button> );
+
+export { Button as default };

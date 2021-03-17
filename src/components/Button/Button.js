6aee038f5ec1e0dacca8b5006acdf3d16d9b4cef
NicholasGray <nicholas_gray@live.com>
2021-03-17 09:54:52 -0400

    update

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,60 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-//import { Button as ButtonM } from '../../Button';
- 
-function Button(props) {
-   return (
-      <button {...props}>
-        {props.children}
-      </button>
-  );
-}
- 
+import * as PropTypes from 'prop-types';
+import * as React from 'react';
+import ButtonStyles from './Button.styles';
+
+const Button = (props) => (
+  <ButtonStyles {...props}>
+    {props.icon}
+    {props.children}
+  </ButtonStyles>
+);
+
+/* eslint-disable sort-keys */
 Button.propTypes = {
-   children: PropTypes.node,
-   size: PropTypes.oneOf(['small', 'large', 'medium'])
-}
- 
-export default Button;
-//import * as PropTypes from 'prop-types';
-// import * as React from 'react';
-// import ButtonStyles from './Button.styles';
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  type: PropTypes.oneOf(['primary', 'secondary', 'success', 'error', 'warning']),
+  mode: PropTypes.oneOf(['filled', 'ghost', 'minimal', 'flat']),
+  /** @uxpinignoreprop */
+  title: PropTypes.string,
+  /** @uxpinignoreprop */
+  background: PropTypes.string,
+  /**
+   * @uxpinpropname Label
+   */
+  children: PropTypes.string,
+  icon: PropTypes.node,
+  iconDirection: PropTypes.oneOf(['left', 'right']),
+  size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl']),
+  stretched: PropTypes.bool,
+};
+/* eslint-enable sort-keys */
 
-// const Button = (props) => (
-//   <ButtonStyles {...props}>
-//     {props.icon}
-//     {props.children}
-//   </ButtonStyles>
-// );
+Button.defaultProps = {
+  disabled: false,
+  iconDirection: 'left',
+  mode: 'filled',
+  size: 'm',
+  stretched: true,
+  type: 'primary',
+};
 
-// /* eslint-disable sort-keys */
-// Button.propTypes = {
-//   onClick: PropTypes.func,
-//   disabled: PropTypes.bool,
-//   type: PropTypes.oneOf(['primary', 'secondary', 'success', 'error', 'warning']),
-//   mode: PropTypes.oneOf(['filled', 'ghost', 'minimal', 'flat']),
-//   /** @uxpinignoreprop */
-//   title: PropTypes.string,
-//   /** @uxpinignoreprop */
-//   background: PropTypes.string,
-//   /**
-//    * @uxpinpropname Label
-//    */
-//   children: PropTypes.string,
-//   icon: PropTypes.node,
-//   iconDirection: PropTypes.oneOf(['left', 'right']),
-//   size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl']),
-//   stretched: PropTypes.bool,
-// };
-// /* eslint-enable sort-keys */
-
-// Button.defaultProps = {
-//   disabled: false,
-//   iconDirection: 'left',
-//   mode: 'filled',
-//   size: 'm',
-//   stretched: true,
-//   type: 'primary',
-// };
-
-// export { Button as default };
+export { Button as default };

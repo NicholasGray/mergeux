2d8105c269c209c08c12582b7d7809141030296b
NicholasGray <nicholas_gray@live.com>
2021-03-18 11:16:40 -0400

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,37 +3,37 @@ import * as React from 'react';
 import Template from './template';
 
 const Button = () => (
-  <Template/>
+ <Template/> 
 );
 
 /* eslint-disable sort-keys */
-Button.propTypes = {
-  onClick: PropTypes.func,
-  disabled: PropTypes.bool,
-  type: PropTypes.oneOf(['primary', 'secondary', 'success', 'error', 'warning']),
-  mode: PropTypes.oneOf(['filled', 'ghost', 'minimal', 'flat']),
-  /** @uxpinignoreprop */
-  title: PropTypes.string,
-  /** @uxpinignoreprop */
-  background: PropTypes.string,
-  /**
-   * @uxpinpropname Label
-   */
-  children: PropTypes.string,
-  icon: PropTypes.node,
-  iconDirection: PropTypes.oneOf(['left', 'right']),
-  size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl']),
-  stretched: PropTypes.bool,
-};
-/* eslint-enable sort-keys */
+// Button.propTypes = {
+//   onClick: PropTypes.func,
+//   disabled: PropTypes.bool,
+//   type: PropTypes.oneOf(['primary', 'secondary', 'success', 'error', 'warning']),
+//   mode: PropTypes.oneOf(['filled', 'ghost', 'minimal', 'flat']),
+//   /** @uxpinignoreprop */
+//   title: PropTypes.string,
+//   /** @uxpinignoreprop */
+//   background: PropTypes.string,
+//   /**
+//    * @uxpinpropname Label
+//    */
+//   children: PropTypes.string,
+//   icon: PropTypes.node,
+//   iconDirection: PropTypes.oneOf(['left', 'right']),
+//   size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl']),
+//   stretched: PropTypes.bool,
+// };
+// /* eslint-enable sort-keys */
 
-Button.defaultProps = {
-  disabled: false,
-  iconDirection: 'left',
-  mode: 'filled',
-  size: 'm',
-  stretched: true,
-  type: 'primary',
-};
+// Button.defaultProps = {
+//   disabled: false,
+//   iconDirection: 'left',
+//   mode: 'filled',
+//   size: 'm',
+//   stretched: true,
+//   type: 'primary',
+// };
 
 export { Button as default };

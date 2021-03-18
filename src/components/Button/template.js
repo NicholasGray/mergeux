18c845971c0f7388c20a0465cf82ffcd488ae74d
NicholasGray <nicholas_gray@live.com>
2021-03-18 11:02:39 -0400

    Update template.js

--- src/components/Button/template.js
+++ src/components/Button/template.js
@@ -1,5 +1,7 @@
-const Template = function() {
-    return (<button type="button" mode="filled" size="s"> Merge! </button>);
+import * as React from 'react';
+
+const Template = function () {
+  return (<button type="button" mode="filled" size="s"> Merge! </button>);
 };
 
 export { Template as default };
